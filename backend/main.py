fd164373768befe56d1db2412db4043863ede23e
Eshaan <eshaan.joshi07@gmail.com>
2026-05-17 21:50:05 -0500

    Polishing backend architecture

--- backend/main.py
+++ backend/main.py
@@ -6,12 +6,14 @@ app = FastAPI()
 
 games: dict[str, Game] = {}
 
-@app.get("/")
+@app.get("/", response_model=dict)
 def root() -> dict:
+    """Health check endpoint."""
     return {"status": "ok"}
 
 @app.post("/create-game", response_model=Game)
 def create_game_route():
+    """Endpoint that creates a new game and returns its state."""
     new_game = create_game()
     
     games[new_game.game_id] = new_game
@@ -21,6 +23,7 @@ def create_game_route():
 
 @app.post("/answer", response_model=Game)
 def process_answer_route(request: AnswerRequest) -> Game:
+    """Processes user's payload answer and updates the game state accordingly."""
     updated_game = update_game(request.answer, games[request.game_id])
 
     return updated_game
